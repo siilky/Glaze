a1f766e6ec8b7e93e9b99a7ddd0bac53c8f3a34c
hydall <53952438+hydall@users.noreply.github.com>
2026-04-06 23:28:08 +0200

    open image viewer when taping on the image attachment, fix hiding button position

--- src/components/chat/ChatMessage.vue
+++ src/components/chat/ChatMessage.vue
@@ -400,6 +400,13 @@ const copyErrorText = (text) => {
     const cleanText = div.textContent || div.innerText || text;
     copyText(cleanText.trim());
 };
+
+const openImage = (src) => {
+    if (!src) return;
+    window.dispatchEvent(new CustomEvent('trigger-open-image', {
+        detail: { src, name: 'Attachment' }
+    }));
+};
 const layoutMode = computed(() => themeState.chatLayout);
 const showFooter = computed(() => {
     // In bubble layout, meta and actions are hidden, so we only show footer if there are actual controls
@@ -552,7 +559,7 @@ onUnmounted(() => {
                     </template>
                     
                     <div v-if="message.image" class="msg-image-attachment" :class="{ 'image-hidden': message.imageHidden }">
-                        <img :src="message.image" alt="Attached image" />
+                        <img :src="message.image" alt="Attached image" @click.stop="openImage(message.image)" />
                         <div class="image-ctx-toggle" @click.stop="emit('toggle-image-hidden')" :title="message.imageHidden ? 'Include image in context' : 'Exclude image from context'">
                             <svg v-if="message.imageHidden" viewBox="0 0 24 24"><path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78l3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z"/></svg>
                             <svg v-else viewBox="0 0 24 24"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z"/></svg>
@@ -753,6 +760,7 @@ onUnmounted(() => {
     overflow: hidden;
     max-width: 100%;
     position: relative;
+    display: inline-block;
 }
 .msg-image-attachment img {
     max-width: 100%;
@@ -761,6 +769,7 @@ onUnmounted(() => {
     display: block;
     object-fit: cover;
     transition: opacity 0.25s;
+    cursor: pointer;
 }
 .msg-image-attachment.image-hidden img {
     opacity: 0.35;
